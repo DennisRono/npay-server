5505d387c107354cff4b731f7f1f4b64c0931860
Dennis Kibet <dennisrkibet@gmail.com>
2024-11-02 18:43:25 +0300

    created register route

--- npay/my_auth/views.py
+++ npay/my_auth/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello, welcome to my Django app!")
+    return HttpResponse("Hello, welcome to my Django Dennis app!")
+
+
+def register(request):
+    return HttpResponse("Hello, register to nullchemy pay")
